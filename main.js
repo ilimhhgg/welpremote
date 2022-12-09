bdfc60f8c2050d3d1f9482d4cb1c975b34adb104
ilimhhgg <74346908+ilimhhgg@users.noreply.github.com>
2022-12-09 21:34:09 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 $('.telegram-form').on('submit', function (event) {
 
     event.stopPropagation();
@@ -15,8 +14,11 @@ $('.telegram-form').on('submit', function (event) {
 
     data.append( 'name', 		$('[name="name"]', form).val() );
     data.append( 'phone', 		$('[name="phone"]', form).val() );
- 
+    data.append( 'email', 		$('[name="email"]', form).val() );
+    data.append( 'text', 		$('[name="text"]', form).val() );
+    data.append( 'file', 		$('[name="file"]', form).val() );
    
+
     files.each(function (key, file) {
         let cont = file.files;
         if ( cont ) {
@@ -51,7 +53,7 @@ $('.telegram-form').on('submit', function (event) {
             return myXhr;
         },
         error: function( jqXHR, textStatus ) {
-            console.log('sdf');
+            // Тут выводим ошибку
         },
         complete: function() {
             // Тут можем что-то делать ПОСЛЕ успешной отправки формы
@@ -62,89 +64,3 @@ $('.telegram-form').on('submit', function (event) {
 
     return false;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let slide_content = document.querySelector('#slide-content')
-
-let signin_form = document.querySelector('#signin-form')
-
-let signin_btn = document.querySelector('#signin-btn')
-
-let darkmode_toggle = document.querySelector('#darkmode-toggle')
-
-let slide_index = 0
-
-slide = () => {
-    let slide_items = slide_content.querySelectorAll('img')
-    slide_items.forEach(e => e.classList.remove('active'))
-    slide_index = slide_index + 1 === slide_items.length ? 0 : slide_index + 1
-    slide_items[slide_index].classList.add('active')
-}
-
-setInterval(slide, 2000)
-
-// animate input
-document.querySelectorAll('.animate-input').forEach(e => {
-    let input = e.querySelector('input')
-    let button = e.querySelector('button')
-
-    input.onkeyup = () => {
-        if (input.value.trim().length > 0) {
-            e.classList.add('active')
-        } else {
-            e.classList.remove('active')
-        }
-
-        // if (checkSigninInput()) {
-        //     signin_btn.removeAttribute('disabled')
-        // } else {
-        //     signin_btn.setAttribute('disabled', 'true')
-        // }
-    }
-
-    // show password button
-    if (button) {
-        button.onclick = () => {
-            if (input.getAttribute('type') === 'text') {
-                input.setAttribute('type', 'password')
-                button.innerHTML = 'Show'
-            } else {
-                input.setAttribute('type', 'text')
-                button.innerHTML = 'Hide'
-            }
-        }
-    }
-})
-
-checkSigninInput = () => {
-    let inputs = signin_form.querySelectorAll('input')
-    return Array.from(inputs).every(input => {
-        return input.value.trim().length >= 6
-    })
-}
-
-// darkmode toggle
-darkmode_toggle.onclick = (e) => {
-    e.preventDefault()
-    let body = document.querySelector('body')
-    body.classList.toggle('dark')
-    darkmode_toggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode'
-}
-
-
-
-//yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
-
-
